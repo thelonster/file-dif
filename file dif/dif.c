6077bb69168f59b5febfe8a00e4f23d629e88dcc
thelonster <lg3hansen@gmail.com>
2017-05-03 17:29:12 -0700

    added difroutine function.

--- file dif/dif.c
+++ file dif/dif.c
@@ -1,10 +1,33 @@
 #include "dif.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define MAXLINE 500
 
 int getline(char *line, int max, FILE *file) {
 	if (fgets(line, max, file) == NULL)
 		return 0;
 	else
 		return strlen(line);
+}
+
+void difroutine() {
+	FILE *fp1, *fp2;
+	fp1 = fopen("file1.txt", "r+");	//Temporary Files
+	fp2 = fopen("file2.txt", "r+");	//Will add a way to choose files later
+	if (fp1 == NULL)
+		fprintf(stderr, "Error opening file\n");
+	if (fp2 == NULL)
+		fprintf(stderr, "Error opening file\n");
+
+	int linenum = 0;
+	char *fp1line = (char*)malloc(MAXLINE);
+	char *fp2line = (char*)malloc(MAXLINE);
+	while (strcmp(fp1line, EOF) != 0 && strcmp(fp2line, EOF) != 0) {
+		getline(fp1line, MAXLINE, fp1);
+		getline(fp2line, MAXLINE, fp2);
+		if (strcmp(fp1line, fp2line) != 0)
+			printf("%d: %s\n%d: %s",linenum,fp1line,linenum++,fp2line);
+	}
 }
--- file dif/dif.h
+++ file dif/dif.h
@@ -1,11 +1,13 @@
 #ifndef dif_h
 #define dif_h
 #define _CRT_NO_SECURE_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
 
 int getline(char*  line, int max, FILE *file);
-
+/*Routine that compares 2 files and prints out files that are different*/
+void difroutine();
 
 
 #endif // !dif_h
