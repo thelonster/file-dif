1b9624f15fbab2ab257086f961adef978ae6572e
thelonster <lg3hansen@gmail.com>
2017-05-09 10:47:53 -0700

    Finished adding flags for different arguments.

--- file dif/main.c
+++ file dif/main.c
@@ -4,8 +4,6 @@
 #include <ctype.h>
 #include <stdlib.h>
 
-
-
 int main(int argc, const char* argv[]) {
 	if (argc < 2) {
 		printf("Error: not enough arguments\n");
@@ -13,10 +11,12 @@ int main(int argc, const char* argv[]) {
 		return 2;
 	}
 	char* fname1, fname2;
+	fname1 = strdup("");
+	fname2 = strdup("");
 	int numf = 0;
 
 	int text = 0, ignorespacechange = 0, ignoreblanklines = 0, ignoretabexpansion = 0,
-		ignorecase = 0, leftcolumn = 0, reportidenticalfiles = 0, recursive = 0,
+		ignorecase = 0, leftcolumn = 0, reportidenticalfiles = 0,
 		suppresscommonlines = 0, width = 0, ignoreallspace = 0, sidebyside = 0;
 
 	for (int a = 0; a < argc; a++) {
@@ -39,8 +39,10 @@ int main(int argc, const char* argv[]) {
 			ignorecase = 1;
 		else if (strcmp(argv[a], "--left-column") == 0)
 			leftcolumn = 1;
-		else if (strcmp(argv[a], "--recursive") == 0 || strcmp(argv[a], "-r") == 0)
-			recursive = 1;
+		else if (strcmp(argv[a], "--recursive") == 0 || strcmp(argv[a], "-r") == 0) {
+			printf("Error: --recursive flag (and comparing directories in general) is not supported in this version of diff.\n");
+			return 0;
+		}			
 		else if (strcmp(argv[a], "--report-identical-files") == 0 || strcmp(argv[a], "-s") == 0)
 			reportidenticalfiles = 1;
 		else if (strcmp(argv[a], "--suppress-common-lines") == 0)
@@ -54,15 +56,28 @@ int main(int argc, const char* argv[]) {
 				return 0;	//Breaks if missing arguments
 			}
 		}
+		else if (strcmp(argv[a], "--ignore-all-space") == 0 || strcmp(argv[a], "-w") == 0)
+			ignoreallspace = 1;
+		else if (strcmp(argv[a], "--side-by-side") == 0 || strcmp(argv[a], "-y") == 0)
+			sidebyside = 1;
 
 	}
+
+	FILE* file1 = fopen(fname1, "r");
+	FILE* file2 = fopen(fname2, "r");
+
 	for (int a = 0; a < argc; a++) {
 		if (strcmp(argv[a], "--version") == 0 || strcmp(argv[a], "-v") == 0)
 			return version();
 		else if (strcmp(argv[a], "--help") == 0)
 			return help();
-		else if (strcmp(argv[a], "--brief") == 0 || strcmp(argv[a], "-q" == 0))
-			;// return brief();
+		else if (strcmp(argv[a], "--brief") == 0 || strcmp(argv[a], "-q" == 0)) {
+			int b = brief(file1, file2, fname1, fname2);
+			if (reportidenticalfiles == 1)
+				if (b == 1)
+					printf("Files %s and %s are identical",fname1,fname2);
+		}
+			
 	}
 	return 0;
 }
